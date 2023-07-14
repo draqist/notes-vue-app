b21c93eeed5081b304ceeb86b6a50733cb43253d
ABDULLAH ABDULFATAH <Boomjay008@gmail.com>
2023-07-15 00:22:26 +0100

    Update TheWelcome.vue time

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -21,6 +21,6 @@ const getNotesByRecentlyUpdated = () => {
     role="list"
     class="divide-y divide-gray-700 overflow-hidden shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
   >
-    <ListNote v-for="note in sortedNotes" :key="note.id" :note="note" />
+    <ListNote v-for="note in sortedNotes.sort((a,b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime() )" :key="note.id" :note="note" />
   </ul>
 </template>
