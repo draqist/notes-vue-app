4cfd8b280071a55ea8074785e225d7c10889324a
Abdullah <Boomjay008@gmail.com>
2023-07-15 12:16:10 +0100

    feat: lint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main class="w-full min-[1280px]:px-8">
+  <main class="w-full overflow-scroll min-[1280px]:pl-4">
     <TheWelcome />
   </main>
 </template>
